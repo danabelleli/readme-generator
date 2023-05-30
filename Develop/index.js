db7a690473fd25780a3e246657dcb85b7927d65d
dannabelleli <belleli.danna@gmail.com>
2023-05-29 19:30:18 -0700

    added the packages that weere required for this app and also started writing code to make the readme file generated and wrote the questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,22 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Your Project's Title?", "Description", "Table of Contents", "Installation", "Usage", "Credits", "License"];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile(`${fileName}`, `${data}\n`, (err) =>
+        // TODO: Describe how this ternary operator works
+        err ? console.error(err) : console.log('Commit logged!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile('readme.md', process.argv[2]);
+}
 
 // Function call to initialize app
 init();
