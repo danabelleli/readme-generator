347112d2526a51605cb2b47801baff443508f6c6
dannabelleli <belleli.danna@gmail.com>
2023-05-29 19:53:43 -0700

    generating readme file function is working but the answeres to the read me questions are not showing up in the readme file yet

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,36 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = ["Your Project's Title?", "Description", "Table of Contents", "Installation", "Usage", "Credits", "License"];
+const questions = [{
+    type: 'input',
+    message: "Your Project's Title?",
+    name: 'title',
+},
+{
+    type: 'input',
+    message: 'Description',
+    name: 'Description',
+},
+{
+    type: 'input',
+    message: 'Table of Contents',
+    name: 'Table of Contents',
+},
+{
+    type: 'input',
+    message: 'Usage',
+    name: 'Usage',
+},
+{
+    type: 'input',
+    message: 'Credits',
+    name: 'Credits',
+},
+{
+    type: 'input',
+    message: 'License',
+    name: 'License',
+}];
 
 
 // TODO: Create a function to write README file
@@ -15,7 +44,10 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    writeToFile('readme.md', process.argv[2]);
+    inquirer
+        .prompt(questions)
+        .then(() => writeToFile('readme.md', process.argv[2])
+        );
 }
 
 // Function call to initialize app
